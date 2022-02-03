205d68abbb90bdeadf4ac32262d059473ccef71e
Amine Dakhli <45517430+aminedakhlii@users.noreply.github.com>
2022-02-03 17:59:10 -0500

    update

--- install.sh
+++ install.sh
@@ -1,3 +1,1 @@
-sudo apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list
-sudo apt-get update && sudo apt-get install elasticsearch
