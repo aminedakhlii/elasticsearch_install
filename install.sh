2c2ad4989dc397f3b7d3fa536664a06693aa645b
aminedakhlii <amine.dakhli@medtech.tn>
2022-02-04 22:09:50 -0500

    cmd

--- install.sh
+++ install.sh
@@ -1,1 +1,3 @@
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add - && sudo apt update && sudo apt install elasticsearch
+
+
+sudo apt-get install apt-transport-https && wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add - && echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list && sudo apt update && sudo apt install elasticsearch
